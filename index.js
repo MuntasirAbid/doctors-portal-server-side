ad94787f1a4545e02de580c0417add9cd070d9a4
MuntasirAbid <muntasiralabid@gmail.com>
2022-11-29 20:02:51 +0100

    posting user info to database, implimented dashboard, getting users booked appointment info from client side

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ async function run() {
     try {
         const appointmentOptionsCollection = client.db('doctorsPortal').collection('appointmentOptions');
         const bookingsCollection = client.db('doctorsPortal').collection('bookings');
+        const usersCollection = client.db('doctorsPortal').collection('users');
 
         app.get('/appointmentOptions', async (req, res) => {
             const date = req.query.date;
@@ -39,6 +40,13 @@ async function run() {
             res.send(options);
         });
 
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        });
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             const query = {
@@ -55,6 +63,12 @@ async function run() {
             }
 
             const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
+        });
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
             res.send(result);
         })
     }
